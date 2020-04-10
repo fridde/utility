cb7abb68d3c4759eab8a6cb8e938ab7aa566bfd0
fridde <f@hehl.se>
2020-04-10 15:03:48 +0200

    Removed superflous function

--- src/Utility.php
+++ src/Utility.php
@@ -223,78 +223,7 @@ class Utility
     }
 
 
-    /**
-     * SUMMARY OF csvstring_to_array
-     *
-     * DESCRIPTION
-     *
-     * @param TYPE ($string, $separatorChar = ',', $enclosureChar = '"', $newlineChar = "\n") ARGDESCRIPTION
-     *
-     * @return TYPE NAME DESCRIPTION
-     */
-    public static function csvstring_to_array($string, $separatorChar = ',', $enclosureChar = '"', $newlineChar = "\n")
-    {
-
-        $array = array();
-        $size = strlen($string);
-        $columnIndex = 0;
-        $rowIndex = 0;
-        $fieldValue = '';
-        $isEnclosured = false;
-        for ($i = 0; $i < $size; $i++) {
-
-            $char = $string{$i};
-            $addChar = '';
-
-            if ($isEnclosured) {
-                if ($char == $enclosureChar) {
-
-                    if ($i + 1 < $size && $string{$i + 1} == $enclosureChar) {
-                        // escaped char
-                        $addChar = $char;
-                        $i++;
-                        // dont check next char
-                    } else {
-                        $isEnclosured = false;
-                    }
-                } else {
-                    $addChar = $char;
-                }
-            } else {
-                if ($char == $enclosureChar) {
-                    $isEnclosured = true;
-                } else {
-
-                    if ($char == $separatorChar) {
-                        $array[$rowIndex][$columnIndex] = $fieldValue;
-                        $fieldValue = '';
-
-                        $columnIndex++;
-                    } elseif ($char == $newlineChar) {
-                        $array[$rowIndex][$columnIndex] = $fieldValue;
-                        $fieldValue = '';
-                        $columnIndex = 0;
-                        $rowIndex++;
-                    } else {
-                        $addChar = $char;
-                    }
-                }
-            }
-            if ($addChar != '') {
-                $fieldValue .= $addChar;
-
-            }
-        }
-
-        if ($fieldValue) {// save last field
-
-            $array[$rowIndex][$columnIndex] = $fieldValue;
-        }
-
-        return $array;
-    }
-
-    /**
+     /**
      * SUMMARY OF remove_whitelines
      *
      * DESCRIPTION
